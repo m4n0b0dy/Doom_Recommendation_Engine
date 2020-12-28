aae1bac2a28ac3810414ee5e36aef7a5da6f2f44
nbody <nbdy@powerserver.lan>
2020-12-28 06:36:31 +0000

    removing from repo

--- notebooks/.ipynb_checkpoints/check-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-es.indices.refresh('production_index')
-print(es.cat.count('production_index', params={"format": "json"}))
